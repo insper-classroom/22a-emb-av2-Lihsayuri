b456055c5382b9cce7567e2089c722c66f622ea3
Lihsayuri <liviasm1@al.insper.edu.br>
2022-05-23 16:28:47 -0300

    AFEC e botão implementado com RTC, bora pros alarmes

--- RTOS-OLED-Xplained-Pro/src/main.c
+++ RTOS-OLED-Xplained-Pro/src/main.c
@@ -36,9 +36,19 @@
 #define BUT_3_IDX 19
 #define BUT_3_IDX_MASK (1u << BUT_3_IDX)
 
+#define AFEC_POT AFEC0
+#define AFEC_POT_ID ID_AFEC0
+#define AFEC_POT_CHANNEL 0 // Canal do pino PD30
+
 /** RTOS  */
 #define TASK_OLED_STACK_SIZE                (1024*6/sizeof(portSTACK_TYPE))
 #define TASK_OLED_STACK_PRIORITY            (tskIDLE_PRIORITY)
+
+#define TASK_ADC_STACK_SIZE (1024*10 / sizeof(portSTACK_TYPE))
+#define TASK_ADC_STACK_PRIORITY (tskIDLE_PRIORITY)
+
+#define TASK_EVENT_STACK_SIZE (1024*10 / sizeof(portSTACK_TYPE))
+#define TASK_EVENT_STACK_PRIORITY (tskIDLE_PRIORITY)
 
 extern void vApplicationStackOverflowHook(xTaskHandle *pxTask,  signed char *pcTaskName);
 extern void vApplicationIdleHook(void);
@@ -46,10 +56,41 @@ extern void vApplicationTickHook(void);
 extern void vApplicationMallocFailedHook(void);
 extern void xPortSysTickHandler(void);
 
+
+typedef struct {
+	uint value;
+} adcData;
+
+typedef struct {
+	int but;
+	int status;
+} adcDataBut;
+
+typedef struct  {
+	uint32_t year;
+	uint32_t month;
+	uint32_t day;
+	uint32_t week;
+	uint32_t hour;
+	uint32_t minute;
+	uint32_t second;
+} calendar;
+
+
+
+/************************************************************************/
+/* globals                                                           */
+/************************************************************************/
+
+QueueHandle_t xQueueADC;
+QueueHandle_t xQueueEvent;
+
 /************************************************************************/
 /* prototypes                                                           */
 /************************************************************************/
 void io_init(void);
+void TC_init(Tc *TC, int ID_TC, int TC_CHANNEL, int freq);
+static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel, afec_callback_t callback);
 
 /************************************************************************/
 /* RTOS application funcs                                               */
@@ -73,20 +114,140 @@ extern void vApplicationMallocFailedHook(void) {
 /************************************************************************/
 
 void but1_callback(void) {
-	printf("1 \n");
+	adcDataBut but1;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+	but1.but = 1;
+	
+	if (pio_get(BUT_1_PIO, PIO_INPUT, BUT_1_IDX_MASK) == 0){
+		but1.status = 1;
+	} else{
+		but1.status = 0;
+	}
+		
+	xQueueSendFromISR(xQueueEvent, &but1, xHigherPriorityTaskWoken);
 }
 
 void but2_callback(void) {
-	printf("2 \n");
+	adcDataBut but2;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+	but2.but = 2;
+	
+	if (pio_get(BUT_2_PIO, PIO_INPUT, BUT_2_IDX_MASK) == 0){
+		but2.status = 1;
+	} else{
+		but2.status = 0;
+	}
+	
+	xQueueSendFromISR(xQueueEvent, &but2, xHigherPriorityTaskWoken);
 }
 
 void but3_callback(void) {
-	printf("3 \n");
+	adcDataBut but3;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+	
+	but3.but = 3;
+	
+	if (pio_get(BUT_3_PIO, PIO_INPUT, BUT_3_IDX_MASK) == 0){
+		but3.status = 1;
+	} else{
+		but3.status = 0;
+	}
+	
+	xQueueSendFromISR(xQueueEvent, &but3, xHigherPriorityTaskWoken);
+}
+
+
+void TC1_Handler(void) {
+	volatile uint32_t ul_dummy;
+
+	ul_dummy = tc_get_status(TC0, 1);
+
+	/* Avoid compiler warning */
+	UNUSED(ul_dummy);
+
+	/* Selecina canal e inicializa conversão */
+	afec_channel_enable(AFEC_POT, AFEC_POT_CHANNEL);
+	afec_start_software_conversion(AFEC_POT);
+}
+
+
+void RTC_Handler(void) {
+	uint32_t ul_status = rtc_get_status(RTC);
+	
+	/* seccond tick */
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
+	}
+	
+	/* Time or date alarm */
+	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
+		// o código para irq de alame vem aqui
+	}
+
+	rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+	rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+}
+
+
+static void AFEC_pot_Callback(void) {
+	adcData adc;
+	adc.value = afec_channel_get_value(AFEC_POT, AFEC_POT_CHANNEL);
+	BaseType_t xHigherPriorityTaskWoken = pdTRUE;
+	xQueueSendFromISR(xQueueADC, &adc, &xHigherPriorityTaskWoken);
 }
 
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
+
+
+static void task_adc(void *pvParameters) {
+	config_AFEC_pot(AFEC_POT, AFEC_POT_ID, AFEC_POT_CHANNEL, AFEC_pot_Callback);
+	TC_init(TC0, ID_TC1, 1, 10);
+	tc_start(TC0, 1);
+	
+	/* Leitura do valor atual do RTC */
+	uint32_t current_hour, current_min, current_sec;
+	uint32_t current_year, current_month, current_day, current_week;
+
+	// variável para recever dados da fila
+	adcData adc;
+
+	while (1) {
+		if (xQueueReceive(xQueueADC, &(adc), 1000)) {
+			rtc_get_date(RTC, &current_year, &current_month, &current_day, &current_week);
+			rtc_get_time(RTC, &current_hour, &current_min, &current_sec);
+			printf("[AFEC ] %02d:%02d:%04d %02d:%02d:%02d %d \n", current_day, current_month, current_year, current_hour, current_min, current_sec, adc.value);
+		} else {
+			printf("Nao chegou um novo dado em 1 segundo \n");
+		}
+	}
+}
+
+static void task_event(void *pvParameters) {
+	// variável para recever dados da fila
+	adcDataBut but;
+	
+	/* Leitura do valor atual do RTC */
+	uint32_t current_hour, current_min, current_sec;
+	uint32_t current_year, current_month, current_day, current_week;
+
+	while (1) {
+		if (xQueueReceive(xQueueEvent, &(but), 1000)) {
+			rtc_get_date(RTC, &current_year, &current_month, &current_day, &current_week);
+			rtc_get_time(RTC, &current_hour, &current_min, &current_sec);
+			printf("[EVENT ] %02d:%02d:%04d %02d:%02d:%02d  %d: %d\n", current_day, current_month, current_year, current_hour, current_min, current_sec, but.but, but.status);
+		} else {
+			printf("Nao chegou um novo dado em 1 segundo \n");
+		}
+	}
+}
 
 static void task_oled(void *pvParameters) {
 	gfx_mono_ssd1306_init();
@@ -100,6 +261,90 @@ static void task_oled(void *pvParameters) {
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
+
+void TC_init(Tc *TC, int ID_TC, int TC_CHANNEL, int freq) {
+	uint32_t ul_div;
+	uint32_t ul_tcclks;
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+
+	pmc_enable_periph_clk(ID_TC);
+
+	tc_find_mck_divisor(freq, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
+	tc_init(TC, TC_CHANNEL, ul_tcclks | TC_CMR_CPCTRG);
+	tc_write_rc(TC, TC_CHANNEL, (ul_sysclk / ul_div) / freq);
+
+	NVIC_SetPriority((IRQn_Type)ID_TC, 4);
+	NVIC_EnableIRQ((IRQn_Type)ID_TC);
+	tc_enable_interrupt(TC, TC_CHANNEL, TC_IER_CPCS);
+}
+
+
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type) {
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_RTC);
+
+	/* Default RTC configuration, 24-hour mode */
+	rtc_set_hour_mode(rtc, 0);
+
+	/* Configura data e hora manualmente */
+	rtc_set_date(rtc, t.year, t.month, t.day, t.week);
+	rtc_set_time(rtc, t.hour, t.minute, t.second);
+
+	/* Configure RTC interrupts */
+	NVIC_DisableIRQ(id_rtc);
+	NVIC_ClearPendingIRQ(id_rtc);
+	NVIC_SetPriority(id_rtc, 4);
+	NVIC_EnableIRQ(id_rtc);
+
+	/* Ativa interrupcao via alarme */
+	rtc_enable_interrupt(rtc,  irq_type);
+}
+
+
+static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
+                            afec_callback_t callback) {
+  /*************************************
+   * Ativa e configura AFEC
+   *************************************/
+  /* Ativa AFEC - 0 */
+  afec_enable(afec);
+
+  /* struct de configuracao do AFEC */
+  struct afec_config afec_cfg;
+
+  /* Carrega parametros padrao */
+  afec_get_config_defaults(&afec_cfg);
+
+  /* Configura AFEC */
+  afec_init(afec, &afec_cfg);
+
+  /* Configura trigger por software */
+  afec_set_trigger(afec, AFEC_TRIG_SW);
+
+  /*** Configuracao específica do canal AFEC ***/
+  struct afec_ch_config afec_ch_cfg;
+  afec_ch_get_config_defaults(&afec_ch_cfg);
+  afec_ch_cfg.gain = AFEC_GAINVALUE_0;
+  afec_ch_set_config(afec, afec_channel, &afec_ch_cfg);
+
+  /*
+  * Calibracao:
+  * Because the internal ADC offset is 0x200, it should cancel it and shift
+  down to 0.
+  */
+  afec_channel_set_analog_offset(afec, afec_channel, 0x200);
+
+  /***  Configura sensor de temperatura ***/
+  struct afec_temp_sensor_config afec_temp_sensor_cfg;
+
+  afec_temp_sensor_get_config_defaults(&afec_temp_sensor_cfg);
+  afec_temp_sensor_set_config(afec, &afec_temp_sensor_cfg);
+
+  /* configura IRQ */
+  afec_set_callback(afec, afec_channel, callback, 1);
+  NVIC_SetPriority(afec_id, 4);
+  NVIC_EnableIRQ(afec_id);
+}
 
 static void configure_console(void) {
 	const usart_serial_options_t uart_serial_options = {
@@ -171,10 +416,30 @@ int main(void) {
 	board_init();
 	configure_console();
 	io_init();
+	
+	calendar rtc_initial = {2018, 3, 19, 12, 15, 45 ,1};
+	RTC_init(RTC, ID_RTC, rtc_initial, RTC_SR_SEC|RTC_SR_ALARM);
+
+	
+	xQueueADC = xQueueCreate(100, sizeof(adcData));
+	  if (xQueueADC == NULL)
+		printf("falha em criar a queue xQueueADC \n");
+
+	xQueueEvent = xQueueCreate(100, sizeof(adcDataBut));
+	if (xQueueEvent == NULL)
+		printf("falha em criar a queue xQueueEvent \n");
 
 	if (xTaskCreate(task_oled, "oled", TASK_OLED_STACK_SIZE, NULL, TASK_OLED_STACK_PRIORITY, NULL) != pdPASS) {
 		printf("Failed to create oled task\r\n");
 	}
+	
+	if (xTaskCreate(task_adc, "ADC", TASK_ADC_STACK_SIZE, NULL, TASK_ADC_STACK_PRIORITY, NULL) != pdPASS) {
+		  printf("Failed to create test ADC task\r\n");
+	}
+
+	if (xTaskCreate(task_event, "EVENT", TASK_EVENT_STACK_SIZE, NULL, TASK_EVENT_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test ADC task\r\n");
+	}	  
 
 	vTaskStartScheduler();
 	while(1){}
